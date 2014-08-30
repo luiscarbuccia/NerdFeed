75dce9c11506f8d5171687c3bd183e14ffc85ce8
looneylu <lcarbuccia@gmail.com>
2014-08-30 14:23:41 -0400

    Initial Commit

---
+++ NerdFeed/NerdFeed/CoursesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoursesTableViewController.h
+//  NerdFeed
+//
+//  Created by Luis Carbuccia on 8/30/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoursesTableViewController : UITableViewController
+
+@end
