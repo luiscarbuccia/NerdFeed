dfa7ec67cc6ee18be451356192db3af9eba2e21b
looneylu <lcarbuccia@gmail.com>
2014-08-31 14:35:07 -0400

    added WebViewController

---
+++ NerdFeed/NerdFeed/WebViewController.h
@@ -0,0 +1,15 @@
+//
+//  WebViewController.h
+//  NerdFeed
+//
+//  Created by Luis Carbuccia on 8/31/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@property (nonatomic) NSURL *URL;
+
+@end
---
+++ NerdFeed/NerdFeed/WebViewController.m
@@ -0,0 +1,54 @@
+//
+//  WebViewController.m
+//  NerdFeed
+//
+//  Created by Luis Carbuccia on 8/31/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+#pragma mark - View Controller Life Cycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void) loadView
+{
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.scalesPageToFit = YES;
+    self.view = webView;
+}
+
+- (void)setURL:(NSURL *)URL
+{
+    _URL = URL;
+    
+    if (!_URL)
+    {
+        NSURLRequest *request =[NSURLRequest requestWithURL:_URL];
+        [(UIWebView *)self.view loadRequest:request];
+    }
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
